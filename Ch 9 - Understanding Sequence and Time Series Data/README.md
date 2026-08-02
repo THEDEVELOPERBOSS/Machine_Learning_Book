31129ad4aed42c07a239d1849a29929999e1c250
THEDEVELOPERBOSS <109550555+THEDEVELOPERBOSS@users.noreply.github.com>
2026-08-01 18:04:25 -0600

    Started Chapter 9

---
+++ Ch 9 - Understanding Sequence and Time Series Data/README.md
@@ -0,0 +1,3 @@
+Chapter 9 
+
+predicitng.py pg 169-
---
+++ Ch 9 - Understanding Sequence and Time Series Data/predicitng.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+
+# Create a time series that has a trend, seasonality and noise
+def plot_series(time, series, format='-', start=0, end=None):
+    plt.plot(time[start:end], series[start:end], format)
+    plt.xlabel("Time")
+    plt.ylabel("Value")
+    plt.grid(True)
+
+def trend(time, slope=0):
+    return slope * time
+
+def seasonal_pattern(season_time):
+    # arbitrary pattern change as wanted
+    return np.where(season_time < 0.4,
+                    np.cos(season_time * 2 * np.pi),
+                    1 / np.exp(3 * season_time))
+def seasonaltiy(time, period, amplitude=1, phase=0):
+    # Repeats the same pattern at each period
+    season_time = ((time + phase) % period) / period
+    return amplitude * seasonal_pattern(season_time)
+
+def noise(time, noise_level=1, seed=None):
+    rnd
